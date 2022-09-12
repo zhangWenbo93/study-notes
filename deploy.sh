177c74006abec05320c90d272328552e5f634e3d
张文勃 <zhangwenbo@zhangwenbodeMacBook-Pro.local>
2022-09-12 23:41:36 +0800

    新增vue原理

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # 应为我本地有两个git，我学习的git命名是git@study.github.com
-git push -f git@github.com/zhangWenbo93/study-notes.git master:gh-pages
+git push -f git@github.com:zhangWenbo93/study-notes.git master:gh-pages
 #git push -f git@github.com:你的git名/你的git项目名.git master:你的git分支
 
 cd -
